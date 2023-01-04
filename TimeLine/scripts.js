bbc92e3b386c4aa2daaa8887f9f4b45d78c9236d
bradley <bradley@bradleymcgill.co.uk>
2023-01-04 00:35:17 +0000

    Middle box is highlighted

--- TimeLine/scripts.js
+++ TimeLine/scripts.js
@@ -2,6 +2,7 @@ let initialMousePosition = 0;
 let canMove = false;
 let currentTimelinePosition = 0;
 let saveScrollPercent = 0;
+let windowCenter = window.innerWidth / 2;
 
 window.onmousedown = event => {
     canMove = true;
@@ -14,7 +15,7 @@ window.onmouseup = () => {
 }
 
 window.onmousemove = event => {
-    if (canMove){
+    if (canMove) {
         let movementDifference = event.clientX - initialMousePosition;
         let movePercentage = 100 * (movementDifference / window.innerWidth);
 
@@ -22,6 +23,14 @@ window.onmousemove = event => {
             Math.min(saveScrollPercent + movePercentage, 30)
         , -55);
 
-        document.getElementById("Timeline").style.transform = `translate(${currentTimelinePosition}%, 50%`;
+        document.getElementById("Timeline").style.transform = `translate(${currentTimelinePosition}%, 0%)`;
+
+        let children = document.getElementById("Timeline").children;
+        for (let i = 0; i < children.length; i++) {
+            children[i].classList.remove("centered");
+            if (children[i].getBoundingClientRect().left <= windowCenter && children[i].getBoundingClientRect().right >= windowCenter) {
+                children[i].classList.add("centered");
+            }
+        }
     }
 }
